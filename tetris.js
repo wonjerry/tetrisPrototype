bbf2c03f0b1deb313f65c4b8448e32c9b3c2fdcd
wonjae <dldnjswo19@gmail.com>
2017-07-09 18:00:47 +0900

    innerlogic
    add rotate func and intersectCheck func.

--- tetris.js
+++ tetris.js
@@ -21,20 +21,20 @@ var BLOCKS = [
   ],
   [
     [0,0,0,0],
-    [1,1,0,0],
     [0,1,1,0],
+    [0,0,1,1],
     [0,0,0,0],
   ],
   [
     [0,0,0,0],
+    [0,0,1,1],
     [0,1,1,0],
-    [1,1,0,0],
     [0,0,0,0],
   ],
   [
-    [1,0,0,0],
-    [1,1,0,0],
-    [1,0,0,0],
+    [0,0,1,0],
+    [0,1,1,0],
+    [0,0,1,0],
     [0,0,0,0],
   ],
   [
@@ -50,8 +50,37 @@ var BLOCKS = [
     [0,0,0,0],
   ]
 ]
+/*지금은 배열을 이용하여 블록을 표시 및 회전 하였지만 전반적인 개발을 일단 마치면 bitmask방식으로 바꿔볼 예정이다.*/
+function rotateRight(block){
+  return [
+    [block[3][0],block[2][0],block[1][0],block[0][0]],
+    [block[3][1],block[2][1],block[1][1],block[0][1]],
+    [block[3][2],block[2][2],block[1][2],block[0][2]],
+    [block[3][3],block[2][3],block[1][3],block[0][3]]
+  ];
+}
 
-document.write(BLOCKS[0]);
+function rotateLeft(block){
+  return [
+    [block[0][3],block[1][3],block[2][3],block[3][3]],
+    [block[0][2],block[1][2],block[2][2],block[3][2]],
+    [block[0][1],block[1][1],block[2][1],block[3][1]],
+    [block[0][0],block[1][0],block[2][0],block[3][0]]
+  ];
+}
+
+function intersectCheck(y,x,block,board){
+  for(var i = 0; i < 4; i++){
+    for(var j = 0; j < 4; j++){
+      if(block[i][j]){
+        if( i + y > BOARD_HEIGHT || j + x > BOARD_WIDTH || j + x < 0 || board[y+j][x+i]){
+          return true; /* 움직였을 때 어떤 물체 또는 board 끝에 겹침을 뜻함*/
+        }
+      }
+    }
+  }
+  return false;
+}
 
 
 
